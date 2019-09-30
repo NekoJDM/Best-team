a6459dca6b4fa33d714de11644d1d135eb486102
NekoJDM <kirito2712@yandex.ua>
2019-09-30 17:31:04 +0300

    new ver

--- project/main.py
+++ project/main.py
@@ -1,57 +1,40 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-import requests
-import time
-from func import start, help
+import telebot;
+#from <filename> import <function1>, <function2>
+
+bot = telebot.TeleBot('943887463:AAG_SqWalzrWa3SmSmjrANf6oJ9QxD6-pzk');
     
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+	if message.text == "Привет":
+		bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+	elif message.text == "/help":
+		bot.send_message(message.from_user.id, "Напиши привет")
+	else:
+		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
-logger = logging.getLogger(__name__)
+name = '';
+surname = '';
+age = 0;
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?");
+        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg');
 
+def get_name(message): #получаем фамилию
+    global name;
+    name = message.text;
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
+    bot.register_next_step_handler(message, get_surnme);
 
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("943887463:AAG_SqWalzrWa3SmSmjrANf6oJ9QxD6-pzk", use_context=True)
-	#bot_bot_chatID = '376768545'
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+def get_surname(message):
+    global surname;
+    surname = message.text;
+    bot.send_message('Сколько тебе лет?');
+    bot.register_next_step_handler(message, get_age);
 
 
-if __name__ == '__main__':
-    main()
-
-
-#edit
+bot.polling(none_stop=True, interval=0)
