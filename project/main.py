7fd7b7fe252f5f6bf9951a647ee6a40f424ac2a6
Masik <makcimpolistuk@gmail.com>
2019-10-04 08:25:56 +0300

    123

--- project/main.py
+++ project/main.py
@@ -31,15 +31,22 @@ def get_surname(message):
 	global surname;
 	surname = message.text;
 	bot.send_message(message.from_user.id,'Сколько тебе лет?');
+	bot.register_next_step_handler(message, geta);
+
+def geta(message):
+	bot.send_message(message.from_user.id,'Сколько тебе лет?');
 	bot.register_next_step_handler(message, get_age);
 
 def get_age(message):
 	global age;
+	age=0;
 	while age == 0: #проверяем что возраст изменился
 		try:
-			 age = int(message.text) #проверяем, что возраст введен корректно
+			age = int(message.text) #проверяем, что возраст введен корректно
 		except Exception:
-			 bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+			bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+			bot.register_next_step_handler(message, geta);
+		break; 
 	keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
 	key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
 	keyboard.add(key_yes); #добавляем кнопку в клавиатуру
@@ -47,4 +54,13 @@ def get_age(message):
 	keyboard.add(key_no);
 	question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
 	bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Запомню : )');
+    elif call.data == "no": bot.send_message(call.message.chat.id, 'NO Запомню : )');
+        #переспрашиваем
+
 bot.polling(none_stop=True, interval=0)
