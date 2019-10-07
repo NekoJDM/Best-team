ebae80035592e95498993806bb28e582af198f21
NekoJDM <55176821+NekoJDM@users.noreply.github.com>
2019-10-07 15:33:22 +0300

    Delete func.py

--- project/func.py
+++
@@ -1,7 +0,0 @@
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
