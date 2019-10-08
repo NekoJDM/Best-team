23a1397fc8c6524c7de89b8e9b726e1fff8206cd
Neurdan <nikolay.pilipchuk@gmail.com>
2019-10-08 09:45:54 +0300

    Added bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import telebot
 from telebot import types
+from gh_menu import gh_menu
+import db_users
+from config import create_menu, Goods
 import config
 
 bot = telebot.TeleBot(config.TOKEN)
@@ -20,7 +23,17 @@ text_messages = {
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+	db_users.check_and_add_user(message)
+
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	markup.row('Сделать заказ')
 	bot.send_message(message.from_user.id, text_messages['start'].format(name=message.from_user.first_name), reply_markup=markup)
+	db_users.set_state(message.from_user.id, config.S_GET_CAT)
 
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	markup.row('Назад')
+	msg = bot.send_message(message.from_user.id, text_messages['help'], reply_markup=markup)
+	bot.register_next_step_handler(msg, send_welcome)
