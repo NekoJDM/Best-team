e687dad98e90236bb25e43bf529e3cf35717bed8
NekoJDM <kirito2712@yandex.ua>
2019-10-08 11:30:12 +0300

    constants

--- config.py
+++ config.py
@@ -1,6 +1,30 @@
 from telebot import types
 
+import db_users
+from gh_menu import gh_menu
 
 TOKEN = '943887463:AAG_SqWalzrWa3SmSmjrANf6oJ9QxD6-pzk'
 
 
+# КОНСТАНТЫ СОСТОЯНИЙ
+S_GET_CAT = 'Список категорий'
+S_CHOOSE_CAT = 'Выбор категории'
+
+# КОНСТАНТЫ КАТЕГОРИЙ
+S_SPECIAL_DRINKS = 'Особые напитки'
+S_COFFEE = 'Кофе'
+S_HOT_DRINKS = 'Горячие напитки'
+
+# КОНСТАНТЫ ТОВАРОВ
+S_CHOOSE_GOOD = 'Выбор товара'
+
+S_LATTE_LAVANDA_SHALFEI = 'Латте Лаванда Шалфей'
+S_RAF_LEMON_PIE = 'Раф Лимонный Пай'
+
+
+S_KAPUCHINO = 'Капучино'
+S_LATTE_MAKIATO = 'Латте Макиато'
+
+
+S_KAKAO = 'Какао'
+S_TEA = 'Чай'
