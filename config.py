9807043681ee68a86291bd21524f8dbf84f65a83
NekoJDM <kirito2712@yandex.ua>
2019-10-09 16:26:02 +0300

    full config

--- config.py
+++ config.py
@@ -28,3 +28,97 @@ S_LATTE_MAKIATO = 'Латте Макиато'
 
 S_KAKAO = 'Какао'
 S_TEA = 'Чай'
+
+
+
+
+
+#ОСОБЫЕ НАПИТКИ
+#list_of_latte = ['Латте Лаванда Шалфей']
+
+
+
+
+def create_menu(mass, back=True):
+	"""
+	This function allows to creat menu of buttons.
+	mass - the list of string
+	back - back button, if true, add a button back. Default back=True
+	"""
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+	if len(mass) == 1:
+		markup.row(mass[0])
+	else:
+		while len(mass) > 0:
+			try:
+				cut = mass[:2]
+				markup.row(cut[0], cut[1])
+				del mass[:2]
+				
+				if len(mass) == 1:
+					markup.row(mass[0])
+					break
+			except:
+				print('WTF')
+	
+
+	if back == True:
+		markup.row('Назад')
+	
+	return markup
+def create_menu1(mass, back=True):
+	"""
+	This function allows to creat menu of buttons.
+	mass - the list of string
+	back - back button, if true, add a button back. Default back=True
+	"""
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+	if len(mass) == 1:
+		markup.row(mass[0])
+	else:
+		while len(mass) > 0:
+			try:
+				cut = mass[:2]
+				markup.row(cut[0], cut[1])
+				del mass[:2]
+				
+				if len(mass) == 1:
+					markup.row(mass[0])
+					break
+			except:
+				print('WTF')
+	
+
+	if back == True:
+		markup.row('Назадд')
+	
+	return markup
+
+class Goods:
+	def __init__(self, bot, message, state):
+		self.bot = bot
+		self.message = message
+		self.state = state
+
+	def get_goods_list(self):
+		"""
+		This function allows to get a list of special drinks
+		"""
+		user_id = self.message.from_user.id
+		mass = list(gh_menu[self.message.text])
+		print(mass)
+		markup = create_menu(mass)
+		self.bot.send_message(user_id, 'Выберите напиток', reply_markup=markup)
+		db_users.set_state(user_id, self.state)
+
+	def get_current_good(self, cat):
+		"""
+		This function allows to get a current good
+		"""
+		user_id = self.message.from_user.id
+		mass = list(gh_menu[cat][self.message.text])
+		markup = create_menu(mass)
+		self.bot.send_message(user_id, 'Выберите размер', reply_markup=markup)
+		db_users.set_state(user_id, self.state)
