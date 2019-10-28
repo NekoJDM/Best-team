f32dce161734036086d9419b3f03521850af8116
Neurdan <nikolay.pilipchuk@gmail.com>
2019-10-28 15:12:05 +0200

    Revert "Wrong"
    This reverts commit 8b33b740f9d7a4bf608d00835bac9722713a5155.

--- bot.py
+++ bot.py
@@ -39,7 +39,6 @@ def send_help(message):
 
 @bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_GET_CAT)
 def get_categories(message):
-
 	user_id = message.from_user.id
 	mass = list(gh_menu.keys())
 	markup = create_menu(mass, back=False)
@@ -47,11 +46,8 @@ def get_categories(message):
 	db_users.set_state(user_id, config.S_CHOOSE_CAT)
 
 
-
-
 @bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_CHOOSE_CAT)
 def choose_categories(message):
-
 	user_id = message.from_user.id
 
 	if message.text == 'Особые напитки':
@@ -64,32 +60,30 @@ def choose_categories(message):
 		db_users.set_state(user_id, config.S_HOT_DRINKS)
 		get_hot_drinks(message)
 
-
-
 @bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_SPECIAL_DRINKS)
 def get_special_drinks(message):
-
+	
 	special_drinks = Goods(bot, message, config.S_CHOOSE_GOOD)
 	special_drinks.get_goods_list()
 
 
 @bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_COFFEE)
 def get_coffee(message):
-
 	coffee = Goods(bot, message, config.S_CHOOSE_GOOD)
 	coffee.get_goods_list()
 
 
 @bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_HOT_DRINKS)
 def get_hot_drinks(message):
-
 	hot_drinks = Goods(bot, message, config.S_CHOOSE_GOOD)
 	hot_drinks.get_goods_list()
 
 
 @bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_CHOOSE_GOOD)
 def choose_good(message):
-
+	"""
+	This function allows to choose a goods
+	"""
 	user_id = message.from_user.id
 	if message.text == "Назад":
 		db_users.set_state(user_id, config.S_CHOOSE_CAT)
@@ -112,68 +106,3 @@ def choose_good(message):
 	elif message.text == 'Чай':
 		db_users.set_state(user_id, config.S_TEA)
 		get_tea(message)
-
-# Функція Пилипчука
-@bot.message_handler(func=lambda message: (db_users.get_current_state(message.from_user.id) == config.S_LATTE_LAVANDA_SHALFEI) or (db_users.get_current_state(message.from_user.id) == config.S_RAF_LEMON_PIE) )
-def choose_good1(message):
-
-	user_id = message.from_user.id
-	if message.text == "Назад":
-		db_users.set_state(user_id, config.S_SPECIAL_DRINKS)
-		message.text = 'Особые напитки'
-		get_special_drinks(message)
-
-# Функція Поліщука
-@bot.message_handler(func=lambda message: (db_users.get_current_state(message.from_user.id) == config.S_KAPUCHINO) or (db_users.get_current_state(message.from_user.id) == config.S_LATTE_MAKIATO) )
-def choose_good2(message):
-	user_id = message.from_user.id
-	if message.text == "Назад":
-		db_users.set_state(user_id, config.S_COFFEE)
-		message.text = 'Кофе'
-		get_special_drinks(message)
-
-
-@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_LATTE_LAVANDA_SHALFEI)
-def get_latte_lavanda_shalfei(message):
-
-	latte_lavanda_shalfei = Goods(bot, message, config.S_LATTE_LAVANDA_SHALFEI)
-	latte_lavanda_shalfei.get_current_good(config.S_SPECIAL_DRINKS)
-
-
-@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_RAF_LEMON_PIE)
-def get_raf_lemon_pie(message):
-
-	raf_lemon_pie = Goods(bot, message, config.S_RAF_LEMON_PIE)
-	raf_lemon_pie.get_current_good(config.S_SPECIAL_DRINKS)
-
-
-@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_KAPUCHINO)
-def get_kapuchino(message):
-
-	kapuchino = Goods(bot, message, config.S_KAPUCHINO)
-	kapuchino.get_current_good(config.S_COFFEE)
-
-
-@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_LATTE_MAKIATO)
-def get_latte_makiato(message):
-
-	latte_makiato = Goods(bot, message, config.S_LATTE_MAKIATO)
-	latte_makiato.get_current_good(config.S_COFFEE)
-
-
-@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_KAKAO)
-def get_kakao(message):
-
-	kakao = Goods(bot, message, config.S_KAKAO)
-	kakao.get_current_good(config.S_HOT_DRINKS)
-
-
-@bot.message_handler(func=lambda message: db_users.get_current_state(message.from_user.id) == config.S_TEA)
-def get_tea(message):
-
-	tea = Goods(bot, message, config.S_TEA)
-	tea.get_current_good(config.S_HOT_DRINKS)
-
-
-
-bot.polling(none_stop=True)
